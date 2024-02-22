b98f097615d80696e3fba2452a7d91becbdf3945
chikhidev <chikhi.dev@gmail.com>
2024-02-22 16:38:21 +0100

    push

--- v2.c
+++ v2.c
@@ -1,13 +1,11 @@
 #include "pipex.h"
 
 void validate_existness_of_command
-(t_tracker *tracker, char *cmd, char **cmd_save)
+(t_tracker *tracker, char *cmd, char **cmd_save, int *found)
 {
-    int found;
     int i;
     char *_dir_;
     
-    found = 0;
     i = 0;
     while (tracker->path[i])
     {
@@ -16,39 +14,43 @@ void validate_existness_of_command
         free(_dir_);
         if (access(*cmd_save, X_OK) == 0)
         {
-            found = 1;
+            *found = 1;
             break ;
         }
         free(*cmd_save);
         i++;
-    }
-    if (!found)
-    {
-        *cmd_save = NULL;
-        ft_printf("%s not found\n", cmd);
-        exit_with_message("Command not found\n", tracker);
     }
 }
 
 void    is_executable_command
 (t_tracker *tracker, char *cmd, char **cmd_save)
 {
-    if (cmd[0] == '/')
+    int found;
+
+    found = 0;
+    if (tracker->is_path_available)
+        validate_existness_of_command(tracker,
+            cmd, cmd_save, &found);
+    if (!found)
     {
-        if (access(cmd, X_OK) != 0)
+        if (access(cmd, F_OK) != 0)
         {
             ft_printf("%s not found\n", cmd);
+            *cmd_save = NULL;
             exit_with_message("Command not found\n", tracker);
         }
+        else if (access(cmd, X_OK) != 0)
+        {
+            *cmd_save = NULL;
+            exit_with_message("No permession to execute this command.\n", tracker);
+        }
         else
+        {
             *cmd_save = cmd;
-    }
-    else if (tracker->is_path_available)
-        validate_existness_of_command(tracker,
-            cmd, cmd_save);
-    else
-    {
-        ft_printf("`%s` not found\n", cmd);
-        exit_with_message("Command not found\n", tracker);
+            return ;
+        }
+        *cmd_save = NULL;
+        ft_printf("%s not found\n", *cmd);
+        exit_with_message(NULL, tracker);
     }
 }
