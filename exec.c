42b32f66e5bf5c07c5bf1c4ee786709d480a8fb7
chikhidev <chikhi.dev@gmail.com>
2024-03-09 02:45:20 +0100

    need to change the logic of the pipe

--- exec.c
+++ exec.c
@@ -10,19 +10,6 @@ void    execute_cmd(t_data *data, t_cmd *cmd)
     exit(1);
 }
 
-void    manage_pipe(t_data *data, t_cmd *cmd)
-{
-    if (pipe(cmd->pipe) == -1)
-        error(data, "Failed to create a pipe", 1);
-    if (cmd->prev)
-        dup2(cmd->prev->pipe[0], 0);
-    else
-        dup2(data->input_file, 0);
-    if (cmd->next)
-        dup2(cmd->pipe[1], 1);
-    else
-        dup2(data->output_file, 1);
-}
 
 void    execute_cmds(t_data *data)
 {
@@ -31,15 +18,7 @@ void    execute_cmds(t_data *data)
     curr_cmd = data->head_cmd;
     while (curr_cmd)
     {
-        manage_pipe(data, curr_cmd);
-        data->child_pid = fork();
-        if (data->child_pid == -1)
-            error(data, "Failed to create a child process", 1);
-        if (data->child_pid == 0)
-            execute_cmd(data, curr_cmd);
-        curr_cmd = curr_cmd->next;
-        write(2, "_______________________________\n", 33);
+
     }
-    write(2, "waiting\n", 9); // debug
     wait(NULL);
 }
