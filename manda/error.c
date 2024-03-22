34df291d39c5a1d089f8110a79882d21021f7649
chikhidev <chikhi.dev@gmail.com>
2024-03-22 11:09:02 +0000

    fixed error logic signal

--- manda/error.c
+++ manda/error.c
@@ -14,5 +14,7 @@ void    error(t_data *data, char *msg)
     write(2, msg, ft_strlen(msg));
     write(2, "/n", 1);
     free_all(data);
+    if (data->status >> 8 == 0)
+        exit(1);
     exit(data->status >> 8);
 }
